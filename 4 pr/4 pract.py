b71f97b849839e252f3edadad43237060739884a
KuZm1ch-O <kuzmich555bbb@gmail.com>
2024-09-19 22:48:15 +0300

    4 pract

---
+++ 4 pr/4 pract.py
@@ -0,0 +1,21 @@
+a=int(input())
+b=int(input())
+while a<=b:
+	print(a)
+	a+=1
+print('------')
+#--------2--------
+a=int(input())
+b=int(input())
+if a>b:
+	while a>b:
+		print(b)
+		b+=1
+	print(a)
+if a<b:
+	while b+1>a:
+		print(b)
+		b-=1
+
+
+	
