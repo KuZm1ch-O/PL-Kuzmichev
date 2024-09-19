333971c97568e0abcc5148e64ad96ec8f52cdd5d
KuZm1ch-O <kuzmich555bbb@gmail.com>
2024-09-19 22:48:08 +0300

    3 pract

--- 3 pr/3 pract.py
+++ 3 pr/3 pract.py
@@ -1,23 +1,44 @@
+#------1.1------
 a=int(input())
 b=int(input())
-while a<=b:
+c=int(input())
+d=[1,2,3]
+if a in d:
 	print(a)
-	a+=1
-print('------')
-#--------2--------
+if b in d:
+	print(b)
+if c in d:
+	print(c)
+#------1.2------
+a=input()
+if a[0]==a[1]:
+	print('ДА')
+else:
+	print('НЕТ')
+#--------2.1-----------
+a=int(input())
+b=int(input())
+if a>b: x=a-b
+if a<b: x=a+b
+if a==b: x=11
+print(x)
+
+#--------2.2------------
+f=int(input())
+k=int(input())
+if f<5 and k>2:
+	R=f+k-1
+if k<2: R=k^2
+if k==2: R=1
+print(R)
+#--------3.1-------
 a=int(input())
 b=int(input())
 if a>b:
-	while a>b:
-		print(b)
-		b+=1
 	print(a)
-if a<b:
-	while b+1>a:
-		print(b)
-		b-=1
-#--------3-------
+else: print(b)
+#---------3.2---------
 a=int(input())
-b=int(input())
-if a>b:
-	
+if a%2==0:
+	print('Число четное')
+else: print('Число нечетное')
