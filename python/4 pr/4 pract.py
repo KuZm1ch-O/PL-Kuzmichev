7fee128b20459128600fa8c0a074d972debdd46f
KuZm1ch-O <kuzmich555bbb@gmail.com>
2024-10-02 20:49:42 +0300

    python

--- python/4 pr/4 pract.py
+++ python/4 pr/4 pract.py
@@ -1,22 +1,31 @@
-a=int(input())
-b=int(input())
+a=int(input('Введите число 1 '))
+b=int(input('Введите число 2 '))
 while a<=b:
 	print(a)
 	a+=1
 print('------')
 #--------2--------
-a=int(input())
-b=int(input())
-if a>b:
-	while a>b:
-		print(b)
-		b+=1
-	print(a)
+a = int(input('Введите число 1 '))
+b = int(input('Введите число 2 '))
+
 if a<b:
-	while b+1>a:
-		print(b)
-		b-=1
+		while a<=b:
+			print(a)
+			a+=1
+else:
+	while a>=b:
+		print(a)
+		a-=1
 
 
-#-----------10-------------
+#-----------9-------------
+n=int(input('Введите кол-во чисел Фибоначчи '))
+a,b=0,1
+summa=0
+for i in range (n):
+	summa+=a
+	a=b
+	b=a+b
+print('Сумма чисел Фибоначчи равна ', summa)
+
 
